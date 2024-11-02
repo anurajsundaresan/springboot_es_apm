4c3b650dcf18d6391e6dd7e61fb14c5e024fd7f8
Anuraj Sundaresan <asundaresan@ironeaglex.com>
2024-11-02 10:21:57 -0400

    initial commit

---
+++ src/main/java/com/example/demo/TestLog4j2.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@SpringBootApplication
+public class TestLog4j2 {
+
+	public static void main(String[] args) {
+		log.error("Hello World!");
+		SpringApplication.run(TestLog4j2.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,57 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+import lombok.extern.slf4j.Slf4j;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+
+@Slf4j
+public class HelloController {
+
+    private final List<String> messages = new ArrayList<>();
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello, Spring Boot!";
+    }
+
+    @GetMapping("/messages")
+    public List<String> getMessages() {
+        return messages;
+    }
+    @GetMapping("/error")
+    public List<String> generateErrorLog() {
+    	log.error("This is a generated error!");
+    	messages.add("this is a test error message");
+        return messages;
+    }
+    @PostMapping("/messages")
+    public String addMessage(@RequestBody String message) {
+        messages.add(message);
+        return "Message added: " + message;
+    }
+
+    @PutMapping("/messages/{index}")
+    public String updateMessage(@PathVariable int index, @RequestBody String updatedMessage) {
+        if (index < messages.size()) {
+            messages.set(index, updatedMessage);
+            return "Message updated at index " + index + ": " + updatedMessage;
+        } else {
+            return "Invalid index";
+        }
+    }
+
+    @DeleteMapping("/messages/{index}")
+    public String deleteMessage(@PathVariable int index) {
+        if (index < messages.size()) {
+            String removedMessage = messages.remove(index);
+            return "Message removed at index " + index + ": " + removedMessage;
+        } else {
+            return "Invalid index";
+        }
+    }
+}
